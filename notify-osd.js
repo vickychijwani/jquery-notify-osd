41fc72e8462d96209bcadf964a96a2afce5d34be
Vicky Chijwani <vickychijwani@gmail.com>
2011-06-20 14:04:17 +0530

    changed fade effect duration

--- notify-osd.js
+++ notify-osd.js
@@ -112,7 +112,7 @@
 		show : function() {
 		    this.set_text(opts.text).set_icon(opts.icon).set_dismissable(opts.dismissable);
 		    
-		    $(this).fadeIn('slow',function(){
+		    $(this).fadeIn('fast',function(){
 			notification = new Region({
 			    top    : notif_obj.offset().top,
 			    left   : notif_obj.offset().left,
@@ -137,7 +137,7 @@
 		},
 		dismiss : function() {
 		    $(document).unbind('mousemove');
-		    $(this).fadeOut('slow',function() {
+		    $(this).fadeOut('fast',function() {
 			$(this).remove();
 			$('.clone').remove();
 		    });
@@ -162,7 +162,7 @@
 
 	    $(document).unbind('mousemove');
 	    if(notif_obj.css('display') != 'none') {
-		notif_obj.fadeOut('slow',function() {
+		notif_obj.fadeOut('fast',function() {
 		    notif_obj.show();
 		});
 	    }
